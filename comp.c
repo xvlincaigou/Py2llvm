cf693cde7520e6a6038a61531d2c182bc1f04a5b
caoye <y-cao21@mails.tsinghua.edu.cn>
2024-12-20 12:40:18 +0800

    finish all

--- comp.c
+++ comp.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-int print(int a) {
-    printf("%d\n", a);
-    return 0;
+// 实现 print_i32 函数，用于打印整数
+void print_i32(int x)
+{
+	printf("%d\n", x);
+}
+
+// 实现 print_str 函数，用于打印字符串
+void print_str(char *s)
+{
+	printf("%s\n", s);
 }
